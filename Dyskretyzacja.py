1a55e0c835da633fd87e4a17ae7c0886827e640d
TheShadowKing19 <mikiwolf19@gmail.com>
2022-04-08 22:08:46 +0200

    Ukończone zadanie + metody

--- Dyskretyzacja.py
+++ Dyskretyzacja.py
@@ -63,17 +63,13 @@ if __name__ == '__main__':
                'spline36', 'hanning', 'hamming', 'hermite', 'kaiser', 'quadric',
                'catrom', 'gaussian', 'bessel', 'mitchell', 'sinc', 'lanczos']
 
-    # Fixing random state for reproducibility
-    np.random.seed(19680801)
-
-    grid = np.random.rand(4, 4)
-
     fig, axs = plt.subplots(nrows=3, ncols=6, figsize=(9, 6),
                             subplot_kw={'xticks': [], 'yticks': []})
-
+    img = plt.imread('ikona.png')
     for ax, interp_method in zip(axs.flat, methods):
-        ax.imshow(grid, interpolation=interp_method, cmap='viridis')
+        ax.imshow(img, interpolation=interp_method, cmap='viridis')
         ax.set_title(str(interp_method))
 
+    plt.savefig('Metody.png')
     plt.tight_layout()
     plt.show()
